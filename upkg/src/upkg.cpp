fc985a9c2cbab20adacf862654a4163516ebfa0f
Jack <jack.wgm@gamil.com>
2021-10-22 16:25:35 +0800

    Update

--- upkg/src/upkg.cpp
+++ upkg/src/upkg.cpp
@@ -1,9 +1,22 @@
 ﻿#include "upkg/upkg.hpp"
+#include <QCryptographicHash>
 
 upkg::upkg(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	// QString blah = QString(QCryptographicHash::hash(("1"), QCryptographicHash::Md5).toHex());
+	// this->setWindowTitle(blah);
+
+/*
+	ui.inputDirEdit->setReadOnly(true);
+	QObject::connect(ui.InputDirBtn, &QPushButton::clicked, [this]() mutable
+	{
+		QString path = QDir::toNativeSeparators(QFileDialog::getExistingDirectory(this, tr("Input Dir"), QDir::currentPath()));
+		ui.inputDirEdit->setText(path);
+	});
+*/
 }
 
 upkg::~upkg()
