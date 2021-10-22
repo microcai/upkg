0736cce00410dfd34808ef2e733bc789ece0d114
Jack <jack.wgm@gamil.com>
2021-10-22 16:34:08 +0800

    Fix path

--- upkg/src/upkg.cpp
+++ upkg/src/upkg.cpp
@@ -14,10 +14,10 @@ upkg::upkg(QWidget *parent)
 	});
 
 	ui.OutputDirEdit->setReadOnly(true);
-	QObject::connect(ui.InputDirBtn, &QPushButton::clicked, [this]() mutable
+	QObject::connect(ui.OutputDirBtn, &QPushButton::clicked, [this]() mutable
 	{
 		QString path = QDir::toNativeSeparators(QFileDialog::getExistingDirectory(this, tr("请选择存放目录"), QDir::currentPath()));
-		ui.InputDirEdit->setText(path);
+		ui.OutputDirEdit->setText(path);
 	});
 }
 
